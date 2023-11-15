c5a9298ca7a1ffdd1344a0c4835a95aa109a91b7
Oscar David Becerra Mejia <oscardbecerram@gmail.com>
2023-11-14 21:09:42 -0500

    Practice - 14/11/2023

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+    return (
+        <div>
+            <h1>Hello Code SandBox</h1>
+            <h2>Start to editing to see some magic happend !!!</h2>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,66 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
+import { useState } from "react";
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  )
+const rootElement = document.getElementById('root')
+
+const Counter= ({contador}) => {
+  console.log('Counter Render :V')
+  return <h1>{contador}</h1>
 }
+const App =(props) => {
+  const [contadorValue, updateContador]  = useState(13)
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
-    </div>
-  )
-}
+  /*
+  const contador = useState(13)
+  const contadorValue = contador[0]
+  const updateContador = contador[1]
+  */
+  console.log('Render?')
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.part} {props.exercise}</p>
-    </div>
-  )
-}
+  const handleOnClick = () => {
+    //updateContador(contadorValue + 1);
+    updateContador((prevContrador) => {
+      return prevContrador +1;
+    });
+  };
 
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const handleOnClickReset =() => {
+    updateContador(0)
   }
 
+  const isEven = contadorValue % 2 === 0
+  const isEvenMessage = isEven ? 'Es Par alv ' : 'No es par olv'
+
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <p>El valor del contador es:</p>
+      <Counter contador={contadorValue} />
+      <p>{isEvenMessage}</p>
+      <button 
+        onClick={handleOnClick}
+        >
+        Incrementar
+      </button>
+      <button 
+        onClick={handleOnClickReset}
+        >
+        Resetiar
+      </button>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, rootElement);
