e523c9fc3a3bc06eb760ae5af222bf12efe3690d
Oscar David Becerra Mejia <oscardbecerram@gmail.com>
2023-11-15 20:12:42 -0500

    v2

--- src/index.js
+++ src/index.js
@@ -3,19 +3,38 @@ import ReactDOM from "react-dom";
 import './styles.css'
 
 const App =() => {
-  const [left, setLeft] = useState(10)
-  const [right, setRight] = useState(20)
+  //const [left, setLeft] = useState(10)
+  //const [right, setRight] = useState(20)
 
+  const[counters, setCounters] = useState({
+    left: 0,
+    right: 0
+  })
+
+  const handleClickLeft = () => {
+    setCounters({
+      left: counters.left +1,
+      right: counters.right
+    })
+  }
+
+  const handleClickRight = () => {
+    setCounters({
+      left: counters.left,
+      right: counters.right +1
+    })
+  }
+  
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
+      {counters.left}
+      <button onClick={handleClickLeft}>
         Left
       </button>
-      <button onClick={() => setRight(left +1)}>
+      <button onClick={handleClickRight}>
         Right
       </button>
-      {right}
+      {counters.right}
     </div>
   )
 }
