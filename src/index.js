86a68ac42bf7e57fe03c6d9a3293a73f3ca5c08d
Oscar David Becerra Mejia <oscardbecerram@gmail.com>
2023-11-16 20:12:35 -0500

    v6

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-const Hello = ({ name, age }) => {
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-      const bornYear = () => new Date().getFullYear() - age
-  
-    return (
-      <div>
-        <p>
-          Hello {name}, you are {age} years old
-        </p>
-  
-        <p>So you were probably born in {bornYear()}</p>
-      </div>
-    )
-  }
-  
-  const App = () => {
-    const name = 'Peter'
-    const age = 10
-  
-    return (
-      <div>
-        <h1>Greetings</h1>
-        <Hello name="Maya" age={26 + 10} />
-        <Hello name={name} age={age} />
-      </div>
-    )
-  }
+const App = () => {
+
+  const [ counter, setCounter ] = useState(0)
+
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering... ', counter)
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => console.log('Clickiado')}>
+       Sumale Uno :V
+      </button>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
