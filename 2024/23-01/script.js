baac4bbc3cdb49c45b61643f25a4116ca158c6e6
Oscar David Becerra Mejia <oscardbecerram@gmail.com>
2024-01-24 01:19:36 -0500

    sumas

--- 2024/23-01/script.js
+++ 2024/23-01/script.js
@@ -6,3 +6,14 @@ var isDeveloper = true
 
 console.log(firstName)
 console.log(lastName)
+
+const sumar = (oper1, oper2) => {
+    console.log(oper1)
+    console.log(oper2)
+
+    return oper1 + oper2
+}
+
+const resultado = sumar(2, 3)
+
+console.log('El resultado de la suma es: ' + resultado)
